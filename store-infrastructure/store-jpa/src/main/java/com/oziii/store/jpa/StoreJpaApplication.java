644fb15db8fda74788bf280660b8e0821d181dc9
0ziii <youngji503@naver.com>
2024-06-18 21:17:23 +0900

    스프링부트 멀티 모듈 구성

---
+++ store-infrastructure/store-jpa/src/main/java/com/oziii/store/jpa/StoreJpaApplication.java
@@ -0,0 +1,15 @@
+package com.oziii.store.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+//@EnableJpaRepositories(basePackages = {"com.oziii.store.jpa"})
+public class StoreJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreJpaApplication.class, args);
+	}
+
+}
