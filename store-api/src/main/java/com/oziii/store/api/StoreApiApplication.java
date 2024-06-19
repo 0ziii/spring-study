48e42618575bc34100333b80892d987f0e1a7e8d
0ziii <youngji503@naver.com>
2024-06-19 23:04:38 +0900

    feat : 멀티 모듈 추가 구성 및 디비 설정

--- store-api/src/main/java/com/oziii/store/api/StoreApiApplication.java
+++ store-api/src/main/java/com/oziii/store/api/StoreApiApplication.java
@@ -2,8 +2,10 @@ package com.oziii.store.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.oziii.store")
 public class StoreApiApplication {
 
     public static void main(String[] args) {
